8cfa77cabbef759794952bc90157699d767fb657
high-prog <dobriyalsarthaksa.8@gmail.com>
2023-02-04 03:15:09 +0530

    search functionalityu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { signOut } from 'firebase/auth'
 import { auth } from '../firebase'
+import { AuthContext } from '../context/AuthContext'
 
 function Navbar() {
+
+  const currentUser = useContext(AuthContext);
+
   return (
     <div className='navbar'>
         <span className='logo'>SxZ Chat</span>
         <div className="user">
-            <img src="https://www.svgrepo.com/show/499764/user.svg" alt="" />
-            <span>John</span>
+            <img src={currentUser.photoURL} alt="" />
+            <span>{currentUser.displayName}</span>
             <button onClick={() => signOut(auth)}>Logout</button>
         </div>
     </div>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import db from '../firebase';
+import { collection, query, where, getDocs } from 'firebase/firestore';
 
 function Search() {
+
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
+
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  }
+
+  const handleSearch = async () => {
+    const q = query(collection(db,"users"), where("displayName", "==", `${username}` ))
+
+    try{
+
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      })
+    }catch(err){
+      setErr(true);
+    }
+  }
+
+
   return (
     <div className='search'>
       <div className="searchForm">
-        <input type="text" name="" id=""  placeholder='Search for a chat'/>
+        <input onKeyDown={handleKey} onChange={(e) => setUsername(e.target.value)} type="text" name="" id=""  placeholder='Search for a chat'/>
       </div>
+      {err && <span>User Not Found</span>}
+      {
+      
+        user && <div className='userChat'>
+        <img src={user.photoURL} alt="" />
+        <div className='userChatInfo'>
+          <span>{user.displayName}</span>
+        </div>
+      </div>
+      }
    
     </div>
   )
